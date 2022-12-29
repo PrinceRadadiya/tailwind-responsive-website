c43054a3ee355b5c0da0e7c1f8a56d5c424a0ce8
prince <radadiyaprince1@gmail.com>
2022-12-29 16:16:04 +0530

    first commit

--- src/component/Section4.jsx
+++ src/component/Section4.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Section4 = () => {
   return (
     <>
-        <div className='w-full py-[10rem] px-4 bg-white '>
+        <div className='w-full py-[10rem] px-16 bg-white '>
             <div className='max-w-[1240px] '>
               
             </div>            
